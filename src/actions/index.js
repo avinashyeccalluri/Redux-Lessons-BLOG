acc8a144f709958f29b63613760fe6f9c4ddd65d
Avinash Yeccalluri <yeccaluriavinash@gmail.com>
2021-06-05 17:52:06 +0530

    completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,47 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from "lodash"
 
-export const fetchPosts = () => async dispatch => {
+
+// // working with function within function
+// function (){
+//   function(id){
+//     return "print result"
+//   }
+// }
+
+// +++++++++++++++++++++++++TO ACCESS THE ABOVE INNER FUNCTION++++++++++++++++++++++++++++
+// const result = ()=>(id)
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+export const fetchPostsAndUsers =()=> async (dispatch, getState) =>{
+  await dispatch(fetchPosts());
+  const uniqueID = _.uniq(_.map(getState().posts, "userId"));
+
+  uniqueID.forEach(id => dispatch(fetchUser(id)))
+}
+
+export const  fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get('/posts');
 
-  dispatch({ type: 'FETCH_POSTS', payload: response });
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
+
+export const fetchUser = id => async(dispatch)  => {
+
+  const response = await jsonPlaceholder.get('/users/'+id);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+
+
+
+// the function is implemented using the memozing
+// export const fetchUser = id =>  dispatch => _fetchUser(id, dispatch);
+
+// // memoize function
+// const _fetchUser = _.memoize(async(id, dispatch)=>{
+//   const response = await jsonPlaceholder.get('/users/'+id);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import postReducer from './postReducer';
+import userReducer from './userReducer'
 
 export default combineReducers({
-  replaceMe: () => 'hi there'
+  posts : postReducer, 
+  users : userReducer
 });
